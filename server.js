8b9121f50d1808eb29f326d39f1c1b33ae511da8
Lilyan <l.ahmetoglu@qimia.de>
2021-02-05 10:43:30 +0300

    fixing errors

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 app.use(express.static("/build"));
 const CLIENT_ID = "9ebab9fa-1b06-4191-a849-5f94590debf7";
 const BASE_URL = "https://app.hubspot.com/oauth/authorize";
-const REDIRECT_URL = "http://localhost:3000/";
-const REDIRECT_URI = "http://localhost:3000/auth-callback";
+const REDIRECT_URL = "http://localhost:3001/";
+const REDIRECT_URI = "http://localhost:3001/auth-callback";
 const SCOPES = "contacts automation";
 const CLIENT_SECRET = "8dcf41be-c88c-4de6-9ce4-dec4b1b45e7a";
 const API_KEY = "5ec222f8-1992-4df1-8d13-0b5cbe6c2b1a";
@@ -197,14 +197,13 @@ app.post("/api/company-qimia", function (req, res) {
     return res.status(200).send(response);
   });
 });
-const publicPath = path.join(__dirname);
-app.use(express.static(__dirname));
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 const port = process.env.PORT || 3001;
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log("Server is listening on port: 3001");
 });
-
--- src/services/server.js
+++ src/services/server.js
@@ -2,8 +2,8 @@ import axios from "axios";
 //const { CLIENT_ID, BASE_URL, SCOPES, CLIENT_SECRET } = process.env;
 const CLIENT_ID = "9ebab9fa-1b06-4191-a849-5f94590debf7";
 const BASE_URL = "https://app.hubspot.com/oauth/authorize";
-const REDIRECT_URL = "http://localhost:3000/";
-const REDIRECT_URI = "http://localhost:3000/auth-callback";
+const REDIRECT_URL = "http://localhost:3001/";
+const REDIRECT_URI = "http://localhost:3001/auth-callback";
 const SCOPES = "contacts automation";
 const CLIENT_SECRET = "8dcf41be-c88c-4de6-9ce4-dec4b1b45e7a";
 class Server {
@@ -25,7 +25,7 @@ class Server {
 
     const headers = {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Origin": "http://localhost:3001",
       "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
       "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
     };
